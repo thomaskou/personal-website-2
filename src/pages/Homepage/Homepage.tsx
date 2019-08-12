69668bc8449394cdcc90c237d495700f8dede4fc
thomaskou00 <thomaskou00@gmail.com>
2019-08-12 00:03:39 -0700

    Update homepage

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 interface IProps {
 }
@@ -7,6 +9,14 @@ const Homepage: React.FC<IProps> = (props: IProps): JSX.Element => {
     return (
         <React.Fragment>
             <div className="background-rails"/>
+
+            <Container>
+                <div className="font-decorative font-size-4 v-margin-xl">
+                    <span className="bg-color-white">See what I've <Link to="/experience">done</Link></span><br/><br/>
+                    <span className="bg-color-white">See what I've <Link to="/projects">made</Link></span><br/><br/>
+                    <span className="bg-color-white">See who I <Link to="/about">am</Link></span>
+                </div>
+            </Container>
         </React.Fragment>
     );
 };

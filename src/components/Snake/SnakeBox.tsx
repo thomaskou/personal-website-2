ac397ae36b0e1f5188ecd2f76557bf9d4b7fcdb9
Thomas Kou <thomaskou00@gmail.com>
2019-08-14 15:51:00 -0700

    Completed snake game

--- src/components/Snake/SnakeBox.tsx
+++ src/components/Snake/SnakeBox.tsx
@@ -1,9 +1,10 @@
-import React, { CSSProperties, RefObject } from "react";
+import React, { RefObject } from "react";
 import { ReactNode } from "react";
+import cloneDeep from "lodash/cloneDeep";
 
 /**
  * ****************************************************************************************************
- * Interfaces
+ * Interfaces and enums
  * ****************************************************************************************************
  */
 
@@ -12,18 +13,29 @@ interface ICoords {
     y: number;
 }
 
+enum SnakeDirections {
+    UP      = "UP",
+    DOWN    = "DOWN",
+    LEFT    = "LEFT",
+    RIGHT   = "RIGHT",
+}
+
 interface IProps {
     endGameCallback: () => void;
 }
 
 interface IState {
-    playing: boolean;
+    grid: ReactNode[][];
+}
 
+interface IGame {
     w: number;
     h: number;
 
     snakeLength?: number;
     snake?: ICoords[];
+    snakeDir?: SnakeDirections;
+
     occupied: boolean[];
     unoccupiedIndices?: number[];
     dot?: ICoords;
@@ -57,6 +69,27 @@ function recreateUnoccupiedIndices(occupied: boolean[]): number[] {
     return unoccupiedIndices;
 }
 
+function createNewGrid(occupied: boolean[], w: number, h: number): ReactNode[][] {
+    const grid: ReactNode[][] = new Array(h);
+    let g: number;
+    for (g = 0; g < w; g++) {
+        grid[g] = new Array(w);
+    }
+
+    let i: number;
+    for (i = 0; i < occupied.length; i++) {
+        const coords: ICoords = indexToCoords(i, w, h);
+        grid[coords.x][coords.y] = (
+            <div
+                key={"snake-tile-" + i}
+                className="snake-box-tile"
+                style={{backgroundColor: occupied[i] ? "white" : "transparent"}}
+            />
+        );
+    }
+    return grid;
+}
+
 function randomUnoccupied(unoccupiedIndices: number[], width: number, height: number): ICoords {
     const arrayIndex: number = Math.floor(Math.random() * unoccupiedIndices.length);
     const unoccupiedIndex: number = unoccupiedIndices[arrayIndex];
@@ -74,26 +107,34 @@ class SnakeBox extends React.Component<IProps, IState> {
 
     private static defaultProps: IProps = {
         endGameCallback: () => {},
-    }
+    };
 
     private readonly containerRef: RefObject<HTMLDivElement>;
+
+    private playing: boolean;
+    private game: IGame;
 
     constructor(props: IProps) {
         super(props);
         this.state = {
-            playing: false,
+            grid: createNewGrid([false], 1, 1),
+        };
+        this.game = {
             w: 1,
             h: 1,
             occupied: [false],
         };
         this.containerRef = React.createRef();
+        this.playing = false;
     }
 
     public componentDidMount(): void {
+        document.addEventListener("keydown", this.pressKey);
         this.newGame();
     }
 
     public componentWillUnmount(): void {
+        document.removeEventListener("keydown", this.pressKey);
         this.endGame();
     }
 
@@ -114,9 +155,10 @@ class SnakeBox extends React.Component<IProps, IState> {
 
         const snakeLength = 5;
         const snake = [{
-            x: Math.round(w/2),
-            y: Math.round(h/2),
+            x: Math.floor(w/2),
+            y: Math.floor(h/2),
         }];
+        const snakeDir = SnakeDirections.UP;
 
         const occupied = (new Array(w * h)).map(() => (false));
         occupied[coordsToIndex(snake[0], w, h)] = true;
@@ -124,32 +166,143 @@ class SnakeBox extends React.Component<IProps, IState> {
         const dot = randomUnoccupied(unoccupiedIndices, w, h);
         occupied[coordsToIndex(dot, w, h)] = true;
 
-        this.setState({
-            w, h, snakeLength, snake, occupied, unoccupiedIndices, dot,
-        }, this.startGame);
+        const grid = createNewGrid(occupied, w, h);
+
+        this.game = {
+            w, h,
+            snakeLength, snake, snakeDir,
+            occupied, unoccupiedIndices, dot,
+        };
+        this.setState({grid}, this.startGame);
     };
 
     private startGame = (): void => {
-        this.setState({
-            playing: true,
-        }, () => {
-            setTimeout(this.tickGame, 100);
-        });
+        this.playing = true;
+        setTimeout(this.tickGame, 50);
+    };
+
+    private setGrid = (index: number, coords: ICoords, on: boolean): void => {
+        const {grid} = this.state;
+        const {occupied, unoccupiedIndices} = this.game;
+
+        if (unoccupiedIndices && grid) {
+            if (on) {
+                occupied[index] = true;
+                unoccupiedIndices.splice(unoccupiedIndices.indexOf(index), 1);
+                grid[coords.x][coords.y] = <div key={"snake-tile-" + index} className="snake-box-tile" style={{backgroundColor: "white"}}/>;
+            } else {
+                occupied[index] = false;
+                unoccupiedIndices.push(index);
+                grid[coords.x][coords.y] = <div key={"snake-tile-" + index} className="snake-box-tile" style={{backgroundColor: "transparent"}}/>;
+            }
+        }
+        this.setState({grid});
+    };
+
+    private setGridIndex = (index: number, on: boolean): void => {
+        const {w, h} = this.game;
+        const coords = indexToCoords(index, w, h);
+        this.setGrid(index, coords, on);
+    };
+
+    private setGridCoords = (coords: ICoords, on: boolean): void => {
+        const {w, h} = this.game;
+        const index = coordsToIndex(coords, w, h);
+        this.setGrid(index, coords, on);
+    };
+
+    private pressKey = (event: any): void => {
+        const currentDir = this.game.snakeDir;
+
+        if (currentDir) {
+            switch (event.key) {
+                case "ArrowUp":
+                    if (currentDir !== SnakeDirections.DOWN) {
+                        this.game = ({...this.game, snakeDir: SnakeDirections.UP});
+                    }
+                    break;
+                case "ArrowDown":
+                    if (currentDir !== SnakeDirections.UP) {
+                        this.game = ({...this.game, snakeDir: SnakeDirections.DOWN});
+                    }
+                    break;
+                case "ArrowLeft":
+                    if (currentDir !== SnakeDirections.RIGHT) {
+                        this.game = ({...this.game, snakeDir: SnakeDirections.LEFT});
+                    }
+                    break;
+                case "ArrowRight":
+                    if (currentDir !== SnakeDirections.LEFT) {
+                        this.game = ({...this.game, snakeDir: SnakeDirections.RIGHT});
+                    }
+                    break;
+            }
+        }
+    };
+
+    private moveSnake = (): void => {
+        const {w, h, snake, snakeDir, occupied, unoccupiedIndices} = this.game;
+        let {snakeLength, dot} = this.game;
+
+        if (snakeLength && snake && snakeDir && unoccupiedIndices && dot) {
+
+            let collectedDot: boolean = false;
+
+            // Add new head coords to front of snake array
+            let newHead = cloneDeep(snake[0]);
+            switch (snakeDir) {
+                case SnakeDirections.UP:    newHead.y--; break;
+                case SnakeDirections.DOWN:  newHead.y++; break;
+                case SnakeDirections.LEFT:  newHead.x--; break;
+                case SnakeDirections.RIGHT: newHead.x++; break;
+            }
+            snake.unshift(newHead);
+
+            // Check dot collision
+            if (dot.x === newHead.x && dot.y === newHead.y) {
+                snakeLength++;
+                dot = randomUnoccupied(unoccupiedIndices, w, h);
+                collectedDot = true;
+                this.setGridCoords(dot, true);
+            }
+
+            // Check wall collision
+            if (newHead.x < 0 || newHead.x >= w || newHead.y < 0 || newHead.y >= h) {
+                this.endGame();
+                return;
+            }
+
+            // Check self collision
+            if (!collectedDot && occupied[coordsToIndex(newHead, w, h)]) {
+                this.endGame();
+                return;
+            }
+
+            // Continue after collision checks
+            this.setGridCoords(newHead, true);
+
+            // Check snake length
+            if (snake.length > snakeLength) {
+                let oldTail = snake[snake.length - 1];
+                this.setGridCoords(oldTail, false);
+                snake.pop();
+            }
+
+        }
+        this.game = {...this.game, snakeLength, dot};
     };
 
     private tickGame = (): void => {
-        if (this.state.playing) {
-            setTimeout(this.tickGame, 100);
+        if (this.playing) {
+            this.moveSnake();
+            setTimeout(this.tickGame, 50);
         } else {
             this.endGame();
         }
     };
 
     private endGame = (): void => {
-        this.state = {
-            ...this.state,
-            playing: false,
-        };
+        this.playing = false;
         // TODO: end game
     };
 
@@ -160,35 +313,10 @@ class SnakeBox extends React.Component<IProps, IState> {
      */
 
     private createSnakeBox = (): ReactNode => {
-        const {w, h, occupied} = this.state;
-        const color = "white";
-
-        const offStyle: CSSProperties = {
-            flex: 1,
-        };
-        const onStyle: CSSProperties = {
-            flex: 1,
-            backgroundColor: color,
-        };
-
-        const grid: ReactNode[][] = new Array(h);
-        let g: number;
-        for (g = 0; g < w; g++) {
-            grid[g] = new Array(w);
-        }
-
-        let i: number;
-        for (i = 0; i < occupied.length; i++) {
-            const coords: ICoords = indexToCoords(i, w, h);
-            if (occupied[i] === true) {
-                grid[coords.x][coords.y] = <div key={"snake-tile-" + i} style={onStyle}/>;
-            } else {
-                grid[coords.x][coords.y] = <div key={"snake-tile-" + i} style={offStyle}/>;
-            }
-        }
+        const {grid} = this.state;
 
         return (
-            <div className="snake-box" style={{color}}>
+            <div className="snake-box" style={{color: "white"}}>
                 {grid.map((col, j) => (
                     <div key={"snake-col-" + j} className="snake-box-col">
                         {col}
--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,31 +1,18 @@
 import React from "react";
 import Snake from "../../components/Snake/Snake";
-// import { Container } from "react-bootstrap";
-// import { Link } from "react-router-dom";
 
 interface IProps {
 }
 
 const Homepage: React.FC<IProps> = (props: IProps): JSX.Element => {
 
-    // const textContainerClasses: string =
-    //     "font-decorative " +                                // Font
-    //     "font-size-25 font-size-md-3 font-size-lg-4 " +     // Responsive font size
-    //     "my-3 my-sm-5 py-md-3 py-lg-5";                     // Responsive margin/padding
-
     return (
         <main className="d-flex">
+
             <div className="background-rails"/>
 
-            {/* <Container>
-                <div className={textContainerClasses}>
-                    <span className="bg-color-white">See where I've <Link to="/experience">worked</Link></span><br/><br/>
-                    <span className="bg-color-white">See what I've <Link to="/projects">made</Link></span><br/><br/>
-                    <span className="bg-color-white">See who I <Link to="/about">am</Link></span>
-                </div>
-            </Container> */}
-
             <Snake/>
+
         </main>
     );
 };
